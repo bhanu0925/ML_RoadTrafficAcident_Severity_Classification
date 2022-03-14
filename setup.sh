007243f63e9c2ee9f747067804eb4f31ceeb2408
bhanu0925 <39095993+bhanu0925@users.noreply.github.com>
2022-03-14 13:39:58 +0530

    updated version and setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,22 @@
 mkdir -p ~/.streamlit/
 echo "\
 [general]\n\
-email = \"your-email@domain.com\"\n\
+email = \"bhanu0925@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 echo "\
+[theme]\n\
+ textColor='#f3eeee'\n\
+ secondaryBackgroundColor='#161414'\n\
+ font='sans serif'\n\
+ primaryColor='#2961a2'\n\
+ backgroundColor='#3b9cd0'\n\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
 " > ~/.streamlit/config.toml
+
+primaryColor="#2961a2"
+backgroundColor="#3b9cd0"
+secondaryBackgroundColor="#161414"
+textColor="#f3eeee"
